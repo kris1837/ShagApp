1028d611632779e0c3332935135d2fcd29beab01
Amirkhan <amirkhan13101998@gmail.com>
2020-05-07 21:19:39 +0600

    Amirkhan2

--- app/src/main/java/kz/sirius/myapplication/entity/AmirkhanItem.java
+++ app/src/main/java/kz/sirius/myapplication/entity/AmirkhanItem.java
@@ -1,4 +1,77 @@
 package kz.sirius.myapplication.entity;
 
+import java.util.Objects;
+
 public class AmirkhanItem {
+    private String _id;
+    private String _header;
+    private String _imageSource;
+    private String _content;
+
+    @Override
+    public String toString() {
+        return "AmirkhanItem{" +
+                "_id='" + _id + '\'' +
+                ", _header='" + _header + '\'' +
+                ", _content='" + _content + '\'' +
+                ", _iamgeSource='" + _imageSource + '\'' +
+                '}';
+    }
+
+
+
+    public String get_imageSource() {
+        return _imageSource;
+    }
+
+    public void set_imageSource(String _iamgeSource) {
+        this._imageSource = _iamgeSource;
+    }
+
+
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String get_header() {
+        return _header;
+    }
+
+    public void set_header(String _header) {
+        this._header = _header;
+    }
+
+    public String get_content() {
+        return _content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AmirkhanItem that = (AmirkhanItem) o;
+        return Objects.equals(_id, that._id) &&
+                Objects.equals(_header, that._header) &&
+                Objects.equals(_content, that._content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_id, _header, _content);
+    }
+
+    public void set_content(String _content) {
+        this._content = _content;
+    }
+
+    public AmirkhanItem(String _header, String _imageSource, String _content) {
+        this._header = _header;
+        this._imageSource = _imageSource;
+        this._content = _content;
+    }
 }
--- app/src/main/java/kz/sirius/myapplication/viewHolders/AmirkhansViewHolder.java
+++ app/src/main/java/kz/sirius/myapplication/viewHolders/AmirkhansViewHolder.java
@@ -1,23 +1,31 @@
 package kz.sirius.myapplication.viewHolders;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import kz.sirius.myapplication.R;
+import kz.sirius.myapplication.entity.AmirkhanItem;
 
-public class KirillsViewHolder extends RecyclerView.ViewHolder {
+public class AmirkhansViewHolder extends RecyclerView.ViewHolder {
 
-    TextView uiNiceText;
+    TextView uiHeaderText;
+    TextView uiContentText;
+    ImageView image;
 
-    public KirillsViewHolder(@NonNull View itemView) {
+    public AmirkhansViewHolder(@NonNull View itemView) {
         super(itemView);
 
-        uiNiceText = itemView.findViewById(R.id.uiNiceText);
+        uiHeaderText = itemView.findViewById(R.id.uiHeaderText);
+        uiContentText = itemView.findViewById(R.id.uiContentText);
     }
 
-    /*public void bind(YourClass object) {
-        uiNiceText.setText();
-    }*/
+    public void bind(AmirkhanItem object) {
+        uiHeaderText.setText(object.get_header());
+        uiContentText.setText(object.get_content());
+        image.setImageResource(R.drawable.ic_assignment_ind_black_24dp);
+    }
 }
