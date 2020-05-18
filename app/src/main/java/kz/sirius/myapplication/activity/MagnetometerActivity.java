cde60a61c371e1638f02d6f7dd99a3dca13359fa
XcenaX <vlad-057@mail.ru>
2020-05-18 21:40:37 +0600

    compass vlad

---
+++ app/src/main/java/kz/sirius/myapplication/activity/MagnetometerActivity.java
@@ -0,0 +1,93 @@
+package kz.sirius.myapplication.activity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+import kz.sirius.myapplication.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MagnetometerActivity extends AppCompatActivity implements SensorEventListener{
+    private ImageView imageView;
+    private SensorManager sensorManager;
+    private float[] mGravity = new float[3];
+    private float[] mGeomagnetic = new float[3];
+    private float azimuth = 0f;
+    private float currentAzimuth = 0f;
+
+    //public static DecimalFormat DECIMAL_FORMATTER;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_magnetometer);
+
+        imageView = (ImageView)findViewById(R.id.compass);
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+
+
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        final float alpha = 0.97f;
+        synchronized (this){
+            if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+                mGravity[0] = alpha*mGravity[0]+(1-alpha)*event.values[0];
+                mGravity[1] = alpha*mGravity[0]+(1-alpha)*event.values[1];
+                mGravity[2] = alpha*mGravity[0]+(1-alpha)*event.values[2];
+            }
+            if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD){
+                mGeomagnetic[0] = alpha*mGeomagnetic[0]+(1-alpha)*event.values[0];
+                mGeomagnetic[1] = alpha*mGeomagnetic[0]+(1-alpha)*event.values[1];
+                mGeomagnetic[2] = alpha*mGeomagnetic[0]+(1-alpha)*event.values[2];
+            }
+
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R,I, mGravity, mGeomagnetic);
+            if(success){
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R,orientation);
+                azimuth = (float)Math.toDegrees(orientation[0]);
+                azimuth = (azimuth+360)%360;
+
+                Animation anim = new RotateAnimation(-currentAzimuth, -azimuth, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                currentAzimuth = azimuth;
+                anim.setDuration(500);
+                anim.setRepeatCount(0);
+                anim.setFillAfter(true);
+
+                imageView.startAnimation(anim);
+
+            }
+        }
+
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
